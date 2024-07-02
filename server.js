10f5eb9cf12f6f4900205466840bad6f7264cc9a
Dianelys Rocha <dianelys@pixelsontarget.com>
2024-07-02 16:43:06 -0400

    didn't work

--- server.js
+++ server.js
@@ -1,36 +1,34 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const socketIo = require('socket.io');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-const PORT = process.env.PORT || 3000;
-
-// Middleware to serve static files from 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Handle socket connection
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('disconnect', (reason) => {
-    console.log(`User disconnected due to ${reason}`);
-  });
-
-  socket.on('error', (error) => {
-    console.error('Socket error:', error);
-  });
-
-  socket.on('ping', () => {
-    socket.emit('pong');
-  });
-  
+app.get('/', function (req, res) {
+    res.send('<h1>Hello world</h1>');
 });
 
-// Start the server
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// When the server receives a post request on /sendData
+app.post('/sendData', function (req, res) {
+
+    //send data to sockets.
+    io.sockets.emit('event', { message: "Hello from server!" })
+    
+    res.send({});
+});
+
+// When a new connection is requested
+io.on('connection', function (socket) {
+    console.log('User Connected!');
+
+    // Send to the connected user
+    socket.emit('event', { message: 'Connected !!!!' });
+    
+    // On each "status", run this function
+    socket.on('status', function (data) {
+        console.log(data);
+    });
+});
+
+// Listen to port 3000
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });

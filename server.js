7e8c9e2a396536e085f0b9be4fb97a8796444c2b
Dianelys Rocha <dianelys@pixelsontarget.com>
2024-07-02 16:00:10 -0400

    something

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
-// This file would contain functions that interact with the ESP32 hardware.
-// Since this is a simulation, details are omitted. Implement according to your hardware interaction needs.
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

b6a10b9fd18ed284266f6cae3de8946a7e86e48c
Dianelys Rocha <dianelys@pixelsontarget.com>
2024-07-02 16:06:31 -0400

    ...

--- server.js
+++ server.js
@@ -1,27 +1,41 @@
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-io.on('connection', (socket) => {
-    console.log('New client connected');
-
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 const PORT = process.env.PORT || 3000;
+
+// Serve a simple test page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Handle socket connection
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Example endpoints to control the LED from external API calls
+  app.get('/led/on', (req, res) => {
+    socket.emit('controlLed', 'turnOnLed');
+    res.send('LED On command sent');
+  });
+
+  app.get('/led/off', (req, res) => {
+    socket.emit('controlLed', 'turnOffLed');
+    res.send('LED Off command sent');
+  });
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+// Start the server
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });

c6c69841773e659550f3d2e05e2db62bd4b4ac29
Dianelys Rocha <dianelys@pixelsontarget.com>
2024-07-02 17:09:33 -0400

    adding web client

--- server.js
+++ server.js
@@ -6,19 +6,32 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+let clients = [];
+
 wss.on('connection', function connection(ws) {
+    clients.push(ws);
     console.log('A client connected');
 
     ws.on('message', function incoming(message) {
-        console.log('Received from ESP32: %s', message);
-        ws.send(`Received your message: ${message}`); // Echo back the received message
+        console.log('Received: %s', message);
+        // Broadcast message to all clients
+        clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        clients = clients.filter(client => client !== ws);
+        console.log('A client disconnected');
     });
 
     ws.send('Hello from server');
 });
 
 app.get('/', (req, res) => {
-    res.send('WebSocket server is running');
+    res.sendFile(__dirname + '/index.html');
 });
 
 const PORT = process.env.PORT || 3000;

7b647cfac0b8f6b39dba457c04c8edf15e7de97f
Dianelys Rocha <dianelys@pixelsontarget.com>
2024-07-02 16:45:09 -0400

    it was even worst

--- server.js
+++ server.js
@@ -1,34 +1,45 @@
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Handle CORS if necessary
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Serve your front-end or send a basic response
 app.get('/', function (req, res) {
     res.send('<h1>Hello world</h1>');
 });
 
-// When the server receives a post request on /sendData
+// Endpoint to receive data from ESP32
 app.post('/sendData', function (req, res) {
-
-    //send data to sockets.
-    io.sockets.emit('event', { message: "Hello from server!" })
-    
+    // Example: emit to all connected sockets
+    io.sockets.emit('event', { message: "Hello from server!" });
     res.send({});
 });
 
-// When a new connection is requested
+// Socket.IO connection handling
 io.on('connection', function (socket) {
     console.log('User Connected!');
 
-    // Send to the connected user
+    // Example: send a message to the connected client
     socket.emit('event', { message: 'Connected !!!!' });
-    
-    // On each "status", run this function
+
+    // Example: handle incoming 'status' events from client
     socket.on('status', function (data) {
         console.log(data);
     });
 });
 
-// Listen to port 3000
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+// Start the server listening on port 3000
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function () {
+    console.log(`Server listening on port ${PORT}`);
 });
